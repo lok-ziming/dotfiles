697395a462bfc19976f153158314c507f627c008
lok <lok-ziming@outlook.com>
2026-01-11 16:28:54 +0800

    修改fish配置

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,14 @@ if status is-interactive
 end
 set fish_greeting ""
 
+if status is-login && test (tty) = "/dev/tty1"
+	if test -z "$WAYLAND_DISPLAY"
+		set -x XDG_SESSION_TYPE wayland
+		set -x XDG_CURRENT_DESKTOP niri
+		exec niri
+	end
+end
+
 function sp 
 	wl-paste | satty -f -
 end
